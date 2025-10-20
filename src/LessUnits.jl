2f178ee5c2dc3a9c785f0a6b92e65a223a659f82
Vasilii Vadimov <vvadimov93@gmail.com>
2025-10-20 13:32:09 +0300

    Basis-specific pseudoinverse calculation

--- src/LessUnits.jl
+++ src/LessUnits.jl
@@ -9,7 +9,7 @@ unravel(a :: Dimension{T}) where {T} = (T => a.power)
 unravel(:: Dimensions{T}) where {T} = map(unravel, T)
 unravel(:: Type{Dimensions{T}}) where {T} = map(unravel, T)
 
-@generated function dim_mat(basis :: Dimensions...)
+@generated function psinv(basis :: Dimensions...)
     index = Dict{Symbol, Int}()
     vecs = Vector{Rational{Int}}[]
     # expand over SI fundamental units
@@ -30,8 +30,34 @@ unravel(:: Type{Dimensions{T}}) where {T} = map(unravel, T)
     for vec in vecs
         append!(vec, zeros(Rational{Int}, length(index) - length(vec)))
     end
+    D = reduce(hcat, vecs)
+    
+    # find pseudoinverse
+    A = D' * D
+    B = Matrix(D')
+    n = size(A)[1]
+    # Gaussian elimination
+    for i in 1 : (n - 1)
+        j = i + argmax(map(abs, A[(i + 1) : end, i]))
+        A[i, :], A[j, :] = A[j, :], A[i, :]
+        B[i, :], B[j, :] = B[j, :], B[i, :]
+        if iszero(A[i, i])
+            return :(throw(ArgumentError("Basis of dimensions `$(dimension.(basis))` is linearly dependent")))
+        end
+        for k in (i + 1) : n
+            tmp = A[k, i] / A[i, i]
+            A[k, i : end] -= tmp * A[i, i : end]
+            B[k, :] -= tmp * B[i, :]
+        end
+    end
+    # solving right-triangular
+    for i in n : -1 : 1
+        B[i, :] /= A[i, i]
+        B[1 : (i - 1), :] -= A[1 : (i - 1), i] * transpose(B[i, :])
+    end
+
     return quote
-        $(index), $(reduce(hcat, vecs))
+        $(index), $(D), $(B)
     end
 end
 
@@ -42,8 +68,7 @@ Returns dimensionless value of quantity `q`, assuming each element of `basis` to
 Throws `ArgumentError` if elements of `basis` are not independent or `q` cannot be expressed through `basis` units.
 """
 @generated function unitless(q :: Quantity, basis :: Tuple{Vararg{Quantity}})
-    index, basis_dim = dim_mat(dimension.(fieldtypes(basis))...)
-    #index, basis_dim = dim_mat(dimension.(basis)...)
+    index, basis_dim, basis_inv = psinv(dimension.(fieldtypes(basis))...)
     q_dim = zeros(Rational{Int}, size(basis_dim)[1])
     for (dim, pow) in unravel(dimension(q))
         if !haskey(index, dim)
@@ -51,31 +76,7 @@ Throws `ArgumentError` if elements of `basis` are not independent or `q` cannot 
         end
         q_dim[index[dim]] = pow
     end
-    # solve system to expand quantity over basis
-    A = basis_dim' * basis_dim
-    b = basis_dim' * q_dim
-    n = length(b)
-    # Gaussian elimination
-    for i in 1 : (n - 1)
-        j = i + argmax(map(abs, A[(i + 1) : end, i]))
-        A[i, :], A[j, :] = A[j, :], A[i, :]
-        b[i], b[j] = b[j], b[i]
-        if iszero(A[i, i])
-            return :(throw(ArgumentError("Basis of dimensions `$(dimension.(basis))` is linearly dependent")))
-        end
-        for k in (i + 1) : n
-            tmp = A[k, i] / A[i, i]
-            A[k, i : end] -= tmp * A[i, i : end]
-            b[k] -= tmp * b[i]
-        end
-    end
-    x = zero(b)
-    # solving right-triangular
-    for i in n : -1 : 1
-        x[i] = b[i] / A[i, i]
-        b[1 : (i - 1)] -= A[1 : (i - 1), i] * x[i]
-    end
-
+    x = basis_inv * q_dim
     if basis_dim * x != q_dim
         return :(throw(ArgumentError("Quantity of dimension `$(dimension(q))` cannot be expanded over `$(dimension.(basis))`")))
     end
